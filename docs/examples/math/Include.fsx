261fb6f432d082d41e15514a4715b9c60997a968
Allister Beharry <allister.beharry@gmail.com>
2026-01-06 08:42:33 -0400

    Add Statistics.fsx script.

--- docs/examples/math/Include.fsx
+++ docs/examples/math/Include.fsx
@@ -6,28 +6,32 @@
 #r "C:\\Users\\Allister\\.nuget\\packages\\mathnet.numerics.fsharp\\4.15.0\\lib\\netstandard2.0\\MathNet.Numerics.FSharp.dll"
 #r @"C:\\Users\\Allister\\.nuget\\packages\\microsoft.z3.x64\4.8.10\\lib\\netstandard1.4\\Microsoft.Z3.dll"
 #r "C:\\Users\\Allister\\.nuget\\packages\\fsharp.quotations.evaluator\\2.1.0\\lib\\netstandard2.0\\FSharp.Quotations.Evaluator.dll"
+#r "C:\\Users\\Allister\\.nuget\\packages\\csvhelper\\12.1.2\\lib\\netstandard2.0\\CsvHelper.dll"
+#r "C:\\Users\\Allister\\.nuget\\packages\\fsharp.interop.dynamic\\4.0.3.130\\lib\\netstandard1.6\\FSharp.Interop.Dynamic.dll"
+
 #r "C:\\Users\\Allister\\.nuget\\packages\\unquote\\7.0.1\\lib\\netstandard2.0\\Unquote.dll"
 #r "C:\\Projects\\Sylvia\\src\\Lang\\Sylvia.Expressions\\bin\\Debug\\net10.0\\Expect.NETStandard.dll"
-#r "C:\\Projects\\Sylvia\\src\\Lang\\Sylvia.Expressions\\bin\\Debug\\net10.0\\Sylvia.Expressions.dll"
-
-
-#r "C:\\Projects\\Sylvia\\src\\Providers\\Sylvia.Provider.Arithmetic\\src\\Sylvia.Provider.Arithmetic.Runtime\\bin\\Release\\netstandard2.0\\Sylvia.Provider.Arithmetic.Runtime.dll"
-#r "C:\\Projects\\Sylvia\\src\\Base\\Sylvia.Collections\\bin\\Debug\\netstandard2.0\\Sylvia.Collections.dll"
-#r "C:\\Projects\\Sylvia\\src\\Math\\CAS\\Sylvia.CAS.Maxima\\bin\\Debug\\net10.0\\MathNet.Symbolics.dll"
-#r "C:\\Projects\\Sylvia\\src\\Math\\CAS\\Sylvia.CAS.Maxima\\bin\\Debug\\net10.0\\Sylvia.CAS.Maxima.dll"
-#r "C:\\Projects\\Sylvia\\src\\Math\\CAS\\Sylvia.CAS.Maxima\\bin\\Debug\\net10.0\\Sylvia.Runtime.dll"
-
-#r "C:\\Projects\\Sylvia\\src\\Math\\Sylvia.AbstractAlgebra\\bin\\Debug\\net10.0\\Sylvia.AbstractAlgebra.dll"
-#r "C:\\Projects\\Sylvia\\src\\Math\\Sylvia.LinearAlgebra\\bin\\Debug\\net10.0\\Sylvia.LinearAlgebra.dll"
-#r "C:\\Projects\\Sylvia\\src\\Math\\Sylvia.RealAnalysis\\bin\\Debug\\net10.0\\Sylvia.RealAnalysis.dll"
-
-
-
-#r "C:\\Projects\\Sylvia\\src\\Visualization\\Sylvia.Visualization.Html\\bin\\Debug\\net10.0\\Sylvia.Visualization.Html.dll"
-
-
 #r "C:\\Projects\\Sylvia\\ext\\FunScript\\src\\main\\FunScript\\bin\\Debug\\netstandard2.0\\FunScript.dll"
 #r "C:\\Projects\\Sylvia\\ext\\FunScript\\src\\main\\FunScript\\bin\\Debug\\netstandard2.0\\FunScript.Interop.NETStandard.dll"
 #r "C:\\Projects\\Sylvia\\ext\\FunScript\\src\\main\\FunScript\\bin\\Debug\\netstandard2.0\\FunScript.Util.dll"
 #r "C:\\Projects\\Sylvia\\ext\\FunScript\\src\\extra\\FunScript.Bindings.JSXGraph\\bin\\Debug\\netstandard2.0\\FunScript.Bindings.Base.dll"
 #r "C:\\Projects\\Sylvia\\ext\\FunScript\\src\\extra\\FunScript.Bindings.JSXGraph\\bin\\Debug\\netstandard2.0\\FunScript.Bindings.JSXGraph.dll"
+
+#r "C:\\Projects\\Sylvia\\src\\Lang\\Sylvia.Expressions\\bin\\Debug\\net10.0\\Sylvia.Expressions.dll"
+#r "C:\\Projects\\Sylvia\\src\\Math\\CAS\\Sylvia.CAS.Maxima\\bin\\Debug\\net10.0\\MathNet.Symbolics.dll"
+#r "C:\\Projects\\Sylvia\\src\\Providers\\Sylvia.Provider.Arithmetic\\src\\Sylvia.Provider.Arithmetic.Runtime\\bin\\Release\\netstandard2.0\\Sylvia.Provider.Arithmetic.Runtime.dll"
+#r "C:\\Projects\\Sylvia\\src\\Base\\Sylvia.Collections\\bin\\Debug\\netstandard2.0\\Sylvia.Collections.dll"
+
+#r "C:\\Projects\\Sylvia\\src\\Math\\CAS\\Sylvia.CAS.Maxima\\bin\\Debug\\net10.0\\Sylvia.CAS.Maxima.dll"
+#r "C:\\Projects\\Sylvia\\src\\Math\\CAS\\Sylvia.CAS.Maxima\\bin\\Debug\\net10.0\\Sylvia.Runtime.dll"
+#r "C:\\Projects\\Sylvia\\src\\data\\Sylvia.Data\\bin\\Debug\\netstandard2.0\\Sylvia.Data.dll"
+#r "C:\\Projects\\Sylvia\\src\\Visualization\\Sylvia.Visualization.Html\\bin\\Debug\\net10.0\\Sylvia.Visualization.Html.dll"
+
+
+#r "C:\\Projects\\Sylvia\\src\\Math\\Sylvia.AbstractAlgebra\\bin\\Debug\\net10.0\\Sylvia.AbstractAlgebra.dll"
+#r "C:\\Projects\\Sylvia\\src\\Math\\Sylvia.LinearAlgebra\\bin\\Debug\\net10.0\\Sylvia.LinearAlgebra.dll"
+#r "C:\\Projects\\Sylvia\\src\\Math\\Sylvia.RealAnalysis\\bin\\Debug\\net10.0\\Sylvia.RealAnalysis.dll"
+#r "C:\\Projects\\Sylvia\\src\\Math\\Sylvia.Statistics\\bin\\Debug\\net10.0\\Sylvia.Statistics.dll"
+
+
+
---
+++ docs/examples/math/Statistics.fsx
@@ -0,0 +1,74 @@
+#load "Include.fsx"
+
+open MathNet.Numerics
+
+open Sylvia
+open Sylvia.Data
+open RealNumbers
+open LinearRegression
+
+"C:\Users\Allister\Documents\School\EC2020\gretlfiles\wooldridge\wage1.csv" |> csv_file |> with_all_field_types<real> |> csv_fields
+let y,x = realvar2 "y" "x"
+let b0,b1,b2 = realconst3 "beta_0" "beta_1" "beta_2"
+
+let salary,roe,salarydol,roedol = realvar4 "salary" "roe" "salarydol" "roedol"
+let wage, educ, exper = realvar3 "wage" "educ" "exper"
+
+let w =  csv_file "C:\Users\Allister\Documents\School\EC2020\gretlfiles\wooldridge\wage1.csv"
+csv_fields w
+
+let wage1 = 
+    w
+    |> samples ["educ"; "wage"] 
+    |> lr (wage == b0 + b1 * educ)
+
+lrser wage1
+let wage2 = w |> samples ["educ"; "exper"; "wage"] |> lr (wage == b0 + b1*educ + b2*exper)
+lrR2 wage2
+
+lrR2 wage2
+
+let tenure = realvar "tenure"
+let b3 = realconst "beta_3"
+let lw = realvar "lw"
+let wage3 = w |> samples ["educ"; "exper"; "tenure"; "wage"] |> lr (wage == b0 + b1*educ + b2*exper + b3*tenure) |> change_vars [lw == ln wage]
+
+let ceosal1 = csv_file "C:\Users\Allister\Downloads\gretlfiles\wooldridge\ceosal1.csv" |> with_all_field_types<float>
+let ceo1 = ceosal1 |> samples ["roe"; "salary"] |> lr (salary == b0 + b1 * roe)
+ceo1.R2
+
+
+let EARNINGS,S,EXP = realvar3  "EARNINGS" "S" "EXP"
+let eawe21 = 
+    csv_file "C:\\Users\Allister\\Downloads\\EAWE21.csv"
+    |> with_all_field_types<real>
+
+let m1 = eawe21 |> samples ["S"; "EARNINGS"] |> lr (EARNINGS == b0 + b1 * S)
+
+m1
+let roedecl = realvar "roedecl"
+
+let nm = ceo1 |> change_vars [
+    //salarydol == 100 * salary
+    roedecl == roe / 100
+]
+
+lromeqn nm
+
+let m2 = eawe21 |> samples ["S"; "EXP"; "EARNINGS"] |> lr (EARNINGS == b0 + b1 * S + b2 * EXP)  
+m2
+m2 |> change_vars [roedecl == S *** 2]
+
+
+
+
+
+lrR2 m2//, lrrss m2, lrse m2, lrsd m2
+
+let alpha =  Distributions.StudentT.InvCDF(0., 1., (real) m2.N, ((1. - 0.05) / 2.))
+
+let alpha2 = Distributions.FisherSnedecor.InvCDF(1., 498., 0.95)
+alpha2
+
+
+//lems witht lm = SimpleLinearRegressionModel(y .= b0 + b1 * x + u + b0, [])
