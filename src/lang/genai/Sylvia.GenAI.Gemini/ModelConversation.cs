6f5b8ada149779242fac0a9a583324545a251cd8
allisterb <allister.beharry@gmail.com>
2026-01-27 06:59:00 -0400

    Add MC methods.

---
+++ src/lang/genai/Sylvia.GenAI.Gemini/ModelConversation.cs
@@ -0,0 +1,146 @@
+﻿namespace Sylvia.GenAI.Gemini;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.VectorData;
+using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Google;
+using Microsoft.SemanticKernel.Connectors.InMemory;
+using Microsoft.SemanticKernel.Embeddings;
+using Google.GenAI;
+
+public class ModelConversation : Runtime
+{
+    public ModelConversation(string modelId, string? embeddingModelId = null, string[]? systemPrompts = null, params IPlugin[]? plugins) : base()
+    {
+        this.modelId = modelId;
+        this.embeddingModelId = embeddingModelId;
+        IKernelBuilder builder = Kernel.CreateBuilder();
+        builder.Services.AddLogging(builder =>
+            builder
+                .SetMinimumLevel(LogLevel.Trace)
+                .AddProvider(loggerProvider)
+            );
+        var apiKey = config?["Model:ApiKey"] ?? throw new Exception();
+        chat = new GoogleAIGeminiChatCompletionService(this.modelId, apiKey, loggerFactory: loggerFactory);
+        chatClient = chat.AsChatClient();
+        // Source - https://stackoverflow.com/a
+        // Posted by Suriya, modified by community. See post 'Timeline' for change history
+        // Retrieved 2026-01-27, License - CC BY-SA 3.0
+        client = (Client?) typeof(GoogleAIGeminiChatCompletionService).GetField("client", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(chatClient) ?? throw new Exception();
+
+        if (this.embeddingModelId is not null)
+        {
+            builder.AddGoogleAIEmbeddingGenerator(this.embeddingModelId, apiKey);
+        }
+        promptExecutionSettings = new GeminiPromptExecutionSettings()
+        {
+            ModelId = this.modelId,
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(autoInvoke: true),
+            ToolCallBehavior = GeminiToolCallBehavior.AutoInvokeKernelFunctions,
+        };
+        Info("Using Google Gemini model {0}.", this.modelId);
+        builder.Services
+            .AddChatClient(chatClient)
+            .UseFunctionInvocation(loggerFactory)
+            .UseKernelFunctionInvocation(loggerFactory);
+        kernel = builder.Build();
+
+        if (systemPrompts is not null)
+        {
+            foreach (var systemPrompt in systemPrompts)
+            {
+                messages.AddSystemMessage(systemPrompt);
+            }
+        }
+
+        if (plugins is not null)
+        {
+            foreach (var plugin in plugins)
+            {
+                kernel.Plugins.AddFromObject(plugin, plugin.Name);
+            }
+        }
+    }
+
+    #region Methods
+
+    public ModelConversation AddPlugin<T>(string pluginName)
+    {
+        kernel.Plugins.AddFromType<T>(pluginName);
+        return this;
+    }
+
+    public ModelConversation AddPlugin<T>(T obj, string pluginName)
+    {
+#pragma warning disable SKEXP0120 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        kernel.Plugins.AddFromObject<T>(obj, jsonSerializerOptions: new System.Text.Json.JsonSerializerOptions(), pluginName: pluginName);
+#pragma warning restore SKEXP0120 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        return this;
+    }
+
+    public async IAsyncEnumerable<StreamingChatMessageContent> Prompt(string prompt, params object[] args)
+    {
+        var messageItems = new ChatMessageContentItemCollection()
+        {
+            new Microsoft.SemanticKernel.TextContent(string.Format(prompt, args))
+        };
+        messages.AddUserMessage(messageItems);
+        StringBuilder sb = new StringBuilder();
+        await foreach (var m in chat.GetStreamingChatMessageContentsAsync(messages, promptExecutionSettings, kernel))
+        {
+            if (m.Content is not null && !string.IsNullOrEmpty(m.Content))
+            {
+                sb.Append(m.Content);
+                yield return m;
+            }
+        }
+        messages.AddAssistantMessage(sb.ToString());
+    }
+
+    public async IAsyncEnumerable<StreamingChatMessageContent> Prompt(string prompt, byte[]? imageData, string imageMimeType = "image/png")
+    {
+        messages.AddUserMessage([
+            new Microsoft.SemanticKernel.TextContent(prompt),
+            new ImageContent(imageData, imageMimeType),
+
+        ]);
+        StringBuilder sb = new StringBuilder();
+        await foreach (var m in chat.GetStreamingChatMessageContentsAsync(messages, promptExecutionSettings, kernel))
+        {
+            if (m.Content is not null && !string.IsNullOrEmpty(m.Content))
+            {
+                sb.Append(m.Content);
+                yield return m;
+            }
+        }
+        messages.AddAssistantMessage(sb.ToString());
+    }
+    #endregion
+    public readonly string modelId;
+
+    public readonly string? embeddingModelId;
+
+    public readonly Kernel kernel = new Kernel();
+
+    public readonly Client client;
+
+    public readonly IChatClient chatClient;
+
+    public readonly IChatCompletionService chat;
+
+    public readonly ChatHistory messages = new ChatHistory();
+
+    public readonly GeminiPromptExecutionSettings promptExecutionSettings;
+
+    public static IConfigurationRoot? config = null;
+}
