5c3119f2eb815935e848e42d7111a89566a61ba5
allisterb <allister.beharry@gmail.com>
2026-02-07 07:15:27 -0400

    Add ImageGeneratpr tp Gemini project.

---
+++ src/lang/genai/Sylvia.GenAI.Gemini/ImageGenerator.cs
@@ -0,0 +1,25 @@
+﻿namespace Sylvia.GenAI.Gemini;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.GenAI;
+using Google.GenAI.Types;
+using Microsoft.Extensions.Configuration;
+
+public class ImageGenerator : Runtime
+{
+    public ImageGenerator(string apiKey, GenerateImagesConfig config)
+    {
+        client = new Client(apiKey: apiKey);
+        imagesConfig = config;
+    }
+
+    #region Fields
+    public readonly Client client;
+
+    public readonly GenerateImagesConfig imagesConfig;
+
+    public readonly static IConfigurationRoot? config = null;
+    #endregion
+}
